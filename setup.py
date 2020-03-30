b528c57c3f4e5db5ec6a963ba04ae8f371ad28af
Antoine Liutkus <antoine.liutkus@inria.fr>
2020-03-30 09:19:03 +0200

    formatting

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
-
 # Proceed to setup
 setup(
     name='qsketch',
@@ -35,6 +33,6 @@ setup(
     install_requires=[
         'torchsearchsorted @ git+https://github.com/aliutkus/torchsearchsorted',
         'torchpercentile @git+https://github.com/aliutkus/torchpercentile',
-        'torchpercentile @git+https://github.com/aliutkus/torchinterp1d',
+        'torchinterp1d @git+https://github.com/aliutkus/torchinterp1d',
     ],
-    )
+)
